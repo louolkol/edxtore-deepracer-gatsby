3a23d8236428bed85f555f5e187938f5b27f4e5b
louol001 <louolkol@gmail.com>
2021-05-14 15:10:21 +0800

    first commit

---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Path to the logo file on your project
+import edxtoreLogo from '../../../../assets/icon-512x512.png';
+
+const Logo = () => (
+  <img src={edxtoreLogo} alt="Edxtore logo" style={{ width: 180 }} />
+);
+
+export default Logo;
